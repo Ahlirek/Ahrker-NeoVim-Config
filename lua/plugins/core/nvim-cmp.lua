3c778c7161a0bbb0292f9a41d4a0fe48f24e37e1
Ahlirek@outlook.com <sharker92@hotmail.com>
2026-03-18 09:36:47 -0600

    👌 IMPROVE: Cmp with luasnip

--- lua/plugins/core/nvim-cmp.lua
+++ lua/plugins/core/nvim-cmp.lua
@@ -1,13 +1,14 @@
 return {
 	"hrsh7th/nvim-cmp",
 	enabled = true,
-	event = "InsertEnter",
+	event = { "InsertEnter", "CmdlineEnter" },
+	build = "make install_jsregexp",
 	dependencies = {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
-		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
+		"L3MON4D3/LuaSnip",
 		"rafamadriz/friendly-snippets",
 		"lukas-reineke/cmp-under-comparator",
 	},
@@ -32,14 +33,40 @@ return {
 				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<C-e>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.abort()
+					elseif require("luasnip").choice_active() then
+						require("luasnip").change_choice(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if require("luasnip").expand_or_jumpable() then
+						require("luasnip").expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if require("luasnip").jumpable(-1) then
+						require("luasnip").jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
+
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "buffer" },
 				{ name = "path" },
 			}),
+
 			sorting = {
 				comparators = {
 					cmp.config.compare.offset,
@@ -52,6 +79,20 @@ return {
 					cmp.config.compare.order,
 				},
 			},
+		})
+
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = { { name = "buffer" } },
+		})
+
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
 		})
 	end,
 }
